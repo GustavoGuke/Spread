2027d11395782852dacbc47340c8d68f7d7bfb5d
Gustavo Silva <gustavo.athenas13@gmail.com>
2022-04-28 22:16:26 -0300

    Implementado styled-components - Content - Quotes - NarutoImg

---
+++ react/naruto-my-project/src/components/Quotes/Quotes.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export default function Quotes() {
+  return (
+    <div>
+      <p role='p'>quote</p>
+      <button>jutsu</button>
+    </div>
+  )
+}
+
+
+const Content = styled.div`
+
+`
---
+++ react/naruto-my-project/src/pages/App.js
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/aria-role */
+import styled from 'styled-components'
+import Quotes from '../components/Quotes/Quotes';
+import narutoImg from '../images/naruto.png'
+export function App() {
+  return (
+    <Content>
+      <Quotes/>
+      <NarutoImg src={narutoImg} alt='Naruto Uzumaki'/>
+    </Content>
+  );
+}
+
+const Content = styled.div`
+  height: 100vh;
+  padding: 0 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const NarutoImg = styled.img`
+  max-width: 50vw;
+  align-self: flex-end;
+`
