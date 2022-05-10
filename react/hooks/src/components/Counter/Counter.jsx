efac6a8bd621ca420d9cb601f40393baafa1f19b
Gustavo da Silva de Lima <gustavo.lima@poleoduto.com.br>
2022-05-10 14:37:39 -0300

    Contador sem hook-useState

---
+++ react/hooks/src/components/Counter/Counter.jsx
@@ -0,0 +1,40 @@
+
+export default function Counter() {
+    let contador = 0
+    let timer = null
+    let value = null
+
+    function handleClick(e) {
+        e.preventDefault()
+        timer = setInterval(() => {
+            if (contador < value) {
+                contador++
+                document.getElementById('contador').innerHTML = contador
+            }else{
+                if(contador == value) alert('fim do tempo')
+                contador = 0
+                value = -1
+                document.getElementById('contador').innerHTML = contador
+                clearInterval(timer)
+            }
+            
+        }, 1000)
+        timer
+        
+    }
+    function handleChange(e) {
+        value = e.target.value
+    }
+    return (
+        <div>Counter
+            <form >
+                <input id="input" type="number" onChange={handleChange} />
+                <button onClick={handleClick}>start</button>
+            </form>
+
+            <div id="contador">
+                {contador}
+            </div>
+        </div>
+    )
+}
