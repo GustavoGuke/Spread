8a7834d6e4491d4a0f25bb9c6fb455bde00ae5ed
Gustavo Silva <gustavo.athenas13@gmail.com>
2022-05-09 23:01:04 -0300

    ultilização dos hooks state-effect-ref

--- react/naruto-my-project/src/pages/App.js
+++ react/naruto-my-project/src/pages/App.js
@@ -1,23 +1,40 @@
 import styled from 'styled-components'
 import { GlobalStyle } from '../GlobalStyle';
 import { Quotes } from '../components/Quotes/Quotes';
-
+import { useEffect, useRef, useState } from 'react';
 
 import getQuote from '../service/quoteService.js'
 import narutoImg from '../images/naruto.png'
+import jutsuAudio from '../sounds/jutso.mp3'
+
+const audio = new Audio(jutsuAudio)
+
 export function App() {
-  
-  const onUpdate = async ()  => {
+  const isMounted = useRef(true)
+  const [quoteState, setQuoteState] = useState({ quote: 'ok', speaker: 'speaker' })
+
+  const onUpdate = async () => {
     const quote = await getQuote()
-    console.log(quote)
+    if (isMounted) {
+      setQuoteState(quote)
+      audio.play()
+    }
   }
+
+  useEffect(() => {
+    onUpdate()
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
   return (
     <>
-    <GlobalStyle/>
-    <Content>
-      <Quotes  onClick={onUpdate} quote={'teste'} speaker={'speaker'}/>
-      <NarutoImg src={narutoImg} alt='Naruto Uzumaki'/>
-    </Content>
+      <GlobalStyle />
+      <Content>
+        <Quotes {...quoteState} onClick={onUpdate} />
+        <NarutoImg src={narutoImg} alt='Naruto Uzumaki' />
+      </Content>
     </>
   );
 }
