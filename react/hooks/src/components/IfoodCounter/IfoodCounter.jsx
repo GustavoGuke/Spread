45d0f985042f7afb9ecfbc3dc6d9d6b01b2c0a18
Gustavo da Silva de Lima <gustavo.lima@poleoduto.com.br>
2022-05-11 15:12:46 -0300

    Hook useeffect

--- react/hooks/src/components/IfoodCounter/IfoodCounter.jsx
+++ react/hooks/src/components/IfoodCounter/IfoodCounter.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import './index.css'
 
 export default function IfoodCounter() {
   const [value, setValue] = useState(0)
   const [buttonStyle, setButtonStyle] = useState("counter-button-minus-active")
 
+  useEffect(() => {
+    console.log('value mudo')
+  },[value])
   function handleDown(){
-
     if( value <=1){
       setButtonStyle("counter-button-minus-desactive")
     }
