d509c12c32ecd232997eba615c0beaf012c70646
Gustavo da Silva de Lima <gustavo.lima@poleoduto.com.br>
2022-05-11 12:16:58 -0300

    Componente pratico com useState

--- react/hooks/src/App.jsx
+++ react/hooks/src/App.jsx
@@ -1,10 +1,12 @@
 import Counter from "./components/Counter/Counter"
 import CounterHook from "./components/Counter/CounterHook"
+import IfoodCounter from "./components/IfoodCounter/IfoodCounter"
 function App() {
   return (
     <div >
       <Counter />
       <CounterHook/>
+      <IfoodCounter/>
     </div>
   )
 }
---
+++ react/hooks/src/components/IfoodCounter/IfoodCounter.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import './index.css'
+
+export default function IfoodCounter() {
+  const [value, setValue] = useState(0)
+  const [buttonStyle, setButtonStyle] = useState("counter-button-minus-active")
+
+  function handleDown(){
+
+    if( value <=1){
+      setButtonStyle("counter-button-minus-desactive")
+    }
+    if(value > 0){
+      setValue(value-1)
+    }
+  }
+  function handleUp(){
+    setValue(value+1)
+    setButtonStyle("counter-button-minus-active")
+  }
+  return (
+    <div className="counter-wrapper">
+     <button className={buttonStyle} onClick={handleDown}>-</button>
+      <p> {value} </p>
+      <button className="counter-button-plus-active" onClick={handleUp}>+</button>
+    </div>
+  )
+}
