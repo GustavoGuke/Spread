8943b8b4875735947ed9f4bc4e397d19b3bc241f
Gustavo Silva <gustavo.athenas13@gmail.com>
2022-06-29 19:44:31 -0300

    mudando url

--- nodejs/intro_node_pg/js/_database.js
+++ nodejs/intro_node_pg/js/_database.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg')
 
-const connectionString = `postgres://hlyyonpv:Tje_34ji3lyuAdMrwQX2XV6BoWqjcEAW@motty.db.elephantsql.com/hlyyonpv`
+const connectionString = ``
 const client = new Client({
   connectionString,
 })
